806c7ad7ea6534bcf596584aebd84266ba5fa03d
Jason Edwards <jasonedwards2011@gmail.com>
2026-04-19 20:47:47 +0000

    Basic menus

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+from new_army import new_army
+
 def main():
-    print("Hello from simpler-army-builder!")
+    options = {
+        1: "New Army",
+        2: "Load Army",
+        3: "Exit"
+    }
+    answer: int = 0
+    input_text = "Select an option (1-3):\n\t"
+    input_text += "\n\t".join([f"{key}. {value}" for key, value in options.items()])
+    input_text += "\n"
+
+    while answer not in options.keys():
+        answer = input(input_text)
+
+        try:
+            answer = int(answer)
+        except ValueError:
+            answer = 0
+        if answer not in options.keys():
+            print("Invalid option. Please try again.")
+    
+    match answer:
+        case 1:
+            new_army()
+        case 2:
+            print("You selected LOAD ARMY")
+        case 3:
+            print("Exiting...")
 
 
 if __name__ == "__main__":
---
+++ new_army.py
@@ -0,0 +1,4 @@
+
+def new_army():
+    army_name: str = input("Enter a name for your army: ")
+    print(f"You named your army {army_name}")
