eb8da24b4d012c563872c02c26ce9fa45eda767f
Jason Edwards <jasonedwards2011@gmail.com>
2026-04-21 11:05:07 +0000

    started adding tests for warscrolls

---
+++ test_warscroll.py
@@ -0,0 +1,32 @@
+from warscroll import get_user_inputs, new_warscroll,save_warscrolls
+import pytest
+from pytest import MonkeyPatch
+import sys
+import io
+
+name: str = "Clanrats"
+points: int = 150
+is_hero: bool = False
+input_tuple = (name, points, is_hero)
+warscroll_dict = {"name": name, "points": points, "is_hero": is_hero}
+
+def test_get_user_inputs(monkeypatch: MonkeyPatch):
+    mock_args:str = ", ".join([str(x) for x in input_tuple])
+    print(mock_args)
+    # mock_args = "Clanrats, 150, False"
+    monkeypatch.setattr("sys.stdin", io.StringIO(mock_args))
+    result = get_user_inputs()
+
+    assert isinstance(result, tuple)
+    assert result == input_tuple
+
+def test_new_warscroll():
+    result = new_warscroll(name, points, is_hero)
+
+    assert isinstance(result, dict)
+    assert result == warscroll_dict
+
+def test_save_warscroll():
+    save_warscrolls(warscroll_dict)
+
+    assert True
--- warscroll.py
+++ warscroll.py
@@ -1,23 +1,34 @@
 import json
 from prompt_utils import Option, prompt_user
 
-def create_warscroll() -> dict:
-
+def get_user_inputs() -> tuple[str, int, bool]:
     is_valid: bool = False
     while not is_valid:
-        name, points, is_hero = input("Enter the name, points, and if the warscroll is a hero (True / False)").split()
+        user_inputs: str = input("Enter the name, points, and if the warscroll is a hero (True / False)")
+        name, points, is_hero = user_inputs.split(", ")
         try:
             name = str(name)
             points = int(points)
-            is_hero = bool(is_hero)
+            if is_hero.lower() == "true":
+                is_hero = True
+            elif is_hero.lower() == "false":
+                is_hero = False
+            else:
+                ValueError("You must enter True or False to indicate if the warscroll is a hero.")
             is_valid = True
         except (ValueError, AttributeError):
             print("Invalid entry. The name must be a string, points must be an integer, and is_hero a boolean.")
             is_valid = False
+    return (name, points, is_hero)
 
+def new_warscroll(name: str, points: int, is_hero: bool) -> dict:
+    return  {"name": name, "points": points, "is_hero": is_hero}
 
+def create_warscroll() -> dict:
+
+    name, points, is_hero = get_user_inputs()
     warscrolls = load_warscrolls()
-    new_warscroll =  {"name": name, "points": points, "is_hero": is_hero}
+    overwrite: bool = True
 
     if name in warscrolls.keys():
         ans = prompt_user(
@@ -26,18 +37,36 @@ def create_warscroll() -> dict:
                 2: Option("Cancel", lambda: print("Cancelling warscroll creation"))
             }
         )
-        if ans != 1:
-            return warscrolls[name]
-    warscrolls[name] = new_warscroll
-    save_warscrolls(warscrolls)
-    return new_warscroll
+        overwrite = ans == 1
+
+    if not overwrite:
+        warscroll = warscrolls[name]
+    
+    else:
+        warscroll = new_warscroll(name, points, is_hero)
+        warscrolls[name] = warscroll
+        save_warscrolls(warscrolls)
+    
+    return warscroll
 
 
 def save_warscrolls(warscrolls: dict):
     with open("warscrolls.json", "w") as f:
-        f.wrint(json.dumps(warscrolls))
+        f.write(json.dumps(warscrolls))
 
 def load_warscrolls() -> dict:
+    from pathlib import Path
+    if not Path("warscrolls.json").exists():
+        save_warscrolls({})
     with open("warscrolls.json", "r") as f:
         warscrolls = json.load(f)
     return warscrolls
+
+def print_warscrolls():
+    warscrolls = load_warscrolls()
+    from pprint import pprint
+
+    pprint(warscrolls)
+
+if __name__ == "__main__":
+    print_warscrolls()
