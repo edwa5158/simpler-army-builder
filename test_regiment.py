7d8caf7d4f15ef55582f123bca04af036a7a32ea
edwa5158 <jasonedwards2011@gmail.com>
2026-05-02 11:24:21 -0500

    just saving

--- test_regiment.py
+++ test_regiment.py
@@ -12,16 +12,16 @@ unit1 = Unit.from_warscroll(ws1)
 unit2 = Unit.from_warscroll(ws2)
 
 
-
 def test_regiment_add_unit():
     regiment = Regiment("Regiment 00", [])
-    
+
     regiment.add_unit(unit2)
     regiment.add_unit(unit1)
 
     assert len(regiment.units) == 2
     assert regiment.units[0].unit == unit2
     assert regiment.units[1].unit == unit1
+
 
 def test_regiment_init():
     regiment = Regiment("Regiment 00", [unit2, unit1])
@@ -30,8 +30,16 @@ def test_regiment_init():
     assert regiment.units[0].unit == unit2
     assert regiment.units[1].unit == unit1
 
+
 def test_regiment_remove_unit():
-    regiment = Regiment("Regiment 00", [unit2, unit1, unit2,])
+    regiment = Regiment(
+        "Regiment 00",
+        [
+            unit2,
+            unit1,
+            unit2,
+        ],
+    )
 
     assert len(regiment.units) == 3
     assert regiment.units[0].unit == unit2
@@ -44,13 +52,17 @@ def test_regiment_remove_unit():
     assert regiment.units[0].unit == unit1
     assert regiment.units[1].unit == unit2
 
+
 def test_regiment_points():
     regiment = Regiment("Regiment 00", [unit2, unit1])
 
     assert regiment.points == 450
 
+
 def test_regiment_header():
     regiment = Regiment("Regiment 00", [unit2, unit1])
     print(regiment.header())
-    assert regiment.header() == "Regiment 00                                                450 Points"
-
+    assert (
+        regiment.header()
+        == "Regiment 00                                                450 Points"
+    )
